5f1eb8d5458ff67fd647b50ee88b69ae9407bc23
SnehaSamson <snehasidic87@gmail.com>
2024-03-11 17:19:59 +0530

    Date : 11-03-2024, Topic : adding the components to the investment calculator project

--- REACT-ESSENTIALS-PRACTICEPROJECT/01-starting-project/src/App.jsx
+++ REACT-ESSENTIALS-PRACTICEPROJECT/01-starting-project/src/App.jsx
@@ -1,6 +1,33 @@
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialIvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+});
+
+function handleChange(inputIdentifier, newValue) {
+  setUserInput(prevUserInput => {
+      return {
+          ...prevUserInput,
+          [inputIdentifier]: +newValue,
+      }
+  });
+} 
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+    <Header/>
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    <Results input={userInput}/>
+
+    </>
+
   )
 }
 
