77965215740377d18aaafb246a3b206935d65266
SnehaSamson <snehasidic87@gmail.com>
2024-03-26 17:02:34 +0530

    Date : 26-03-2024, Topic : Adding Task, new Task component and debugging the bugs.

--- PRACTICE-Project-Project Management App/01-starting-project/src/components/ProjectsSidebar.jsx
+++ PRACTICE-Project-Project Management App/01-starting-project/src/components/ProjectsSidebar.jsx
@@ -40,10 +40,9 @@ export default function ProjectsSidebar({
                         </li>
 
                         );
-                    }
-                 )}
+                    })}
                 </ul>
             </aside>
         </>
-    )
+    );
 }
